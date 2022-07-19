d04897e3194738ecbbb7406821f55244d3e9f2c3
Spencer Heffley <spheffley@yahoo.com>
2022-07-19 00:36:35 -0400

    Used "refs" to get values from input fields instead of updating a variable onChange and using state

--- 4-UserList/src/components/Users/AddUser/AddUser.js
+++ 4-UserList/src/components/Users/AddUser/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 import styles from "./AddUser.module.css";
 
@@ -6,50 +6,42 @@ import Card from "../../UI/Card/Card";
 import Button from "../../UI/Button/Button";
 
 const AddUser = props => {
-	const [name, setName] = useState("");
-	const [age, setAge] = useState("");
-
-	const nameChangeHandler = e => {
-		// when the name input is changed
-		setName(e.target.value);
-	};
-
-	const ageChangeHandler = e => {
-		// when the age input is changed
-		setAge(e.target.value);
-	};
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
 	const addUserHandler = e => {
 		// when the add user button is clicked
 		e.preventDefault();
+        const enteredName = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
 
-		if (name.trim() && +age > 0 && +age < 1000) {
+		if (enteredName.trim() && +enteredAge > 0 && +enteredAge < 1000) {
 			// make sure both fields are filled
 			props.onSubmit({
 				id: Math.random()
 					.toString(36)
 					.replace(/[^a-z]+/g, ""), //create a unique id with letters
-				name: name,
-				age: parseInt(age), // parseInt() is used to convert string to integer
+				name: enteredName,
+				age: parseInt(enteredAge), // parseInt() is used to convert string to integer
 			});
-
-			setName("");
-			setAge("");
+            // clear the input fields
+            nameInputRef.current.value = "";
+            ageInputRef.current.value = "";
 		} else {
 			// show alert if one or both of the fields are empty or out of range
-			if (!name.trim() && !+age) {
+			if (!enteredName.trim() && !+enteredAge) {
 				// if both fields are empty
 				props.onIncomplete("Please fill out all fields");
-			} else if (!name.trim()) {
+			} else if (!enteredName.trim()) {
 				// if name field is empty
 				props.onIncomplete("Please fill out the name field");
-			} else if (!+age) {
+			} else if (!+enteredAge) {
 				// if age field is empty
 				props.onIncomplete("Please fill out the age field");
-			} else if (+age < 0) {
+			} else if (+enteredAge < 0) {
 				// if age is too low
 				props.onIncomplete("Age cannot be negative");
-			} else if (+age > 999) {
+			} else if (+enteredAge > 999) {
 				// if age is too high
 				props.onIncomplete("Age cannot be greater than 999");
 			}
@@ -64,8 +56,7 @@ const AddUser = props => {
 					<input
 						type="text"
 						name="username"
-						value={name}
-						onChange={nameChangeHandler}
+                        ref={nameInputRef}
 					/>
 				</div>
 				<div className={styles["form-group"]}>
@@ -73,8 +64,7 @@ const AddUser = props => {
 					<input
 						type="number"
 						name="age"
-						value={age}
-						onChange={ageChangeHandler}
+                        ref={ageInputRef}
 					/>
 				</div>
 				<Button type="submit">Add User</Button>
