53ba588e0fa0434686baea8334b23acc6290a776
Spencer Heffley <spheffley@yahoo.com>
2022-07-22 00:54:33 -0400

    Set up context in the application using Providers and consumers

--- 5-Login/src/App.js
+++ 5-Login/src/App.js
@@ -3,16 +3,17 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        const storedLoginInfo = localStorage.getItem("isLoggedIn");
-        if (storedLoginInfo) {
-            setIsLoggedIn(true);
-        }
-    }, []);
+	useEffect(() => {
+		const storedLoginInfo = localStorage.getItem("isLoggedIn");
+		if (storedLoginInfo) {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
 	const loginHandler = (email, password) => {
 		// We should of course check email and password
@@ -29,13 +30,13 @@ function App() {
 	};
 
 	return (
-		<React.Fragment>
-			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+		<AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+			<MainHeader onLogout={logoutHandler} />
 			<main>
 				{!isLoggedIn && <Login onLogin={loginHandler} />}
 				{isLoggedIn && <Home onLogout={logoutHandler} />}
 			</main>
-		</React.Fragment>
+		</AuthContext.Provider>
 	);
 }
 
