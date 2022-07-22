7aa1beeff1dc2f17cc8327e63905f051223fff09
Spencer Heffley <spheffley@yahoo.com>
2022-07-22 16:03:37 -0400

    Created a reusable input component and used it in Login.js

---
+++ 5-Login/src/components/UI/Input/Input.js
@@ -0,0 +1,18 @@
+import styles from "./Input.module.css";
+
+const Input = props => {
+	return (
+		<div className={props.className}>
+			<label htmlFor={props.id}>{props.label}</label>
+			<input
+				type={props.type}
+				id={props.id}
+				value={props.value}
+				onChange={props.onChange}
+				onBlur={props.onBlur}
+			/>
+		</div>
+	);
+};
+
+export default Input;
