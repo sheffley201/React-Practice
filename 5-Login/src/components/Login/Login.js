d860bb5fecbdf307bacfb8952721bfb638f6f237
Spencer Heffley <spheffley@yahoo.com>
2022-07-22 15:30:29 -0400

    Added auth state management to auth-context file and used functions in that file instead of passing them as props to child components

--- 5-Login/src/components/Login/Login.js
+++ 5-Login/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
 	if (action.type === "USER_INPUT") {
@@ -24,6 +25,7 @@ const passwordReducer = (state, action) => {
 
 const Login = props => {
 	const [formIsValid, setFormIsValid] = useState(false);
+	const context = useContext(AuthContext);
 
 	const [emailState, dispatchEmail] = useReducer(emailReducer, {
 		value: "",
@@ -64,7 +66,7 @@ const Login = props => {
 
 	const submitHandler = event => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		context.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
