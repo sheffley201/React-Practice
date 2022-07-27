b57d784b1feb03fd182cda2daca214019832c526
Spencer Heffley <spheffley@yahoo.com>
2022-07-27 01:21:43 -0400

    Set up reducer for updating cart state created basic function for handling adding items to cart

--- 6-FoodOrderApp/src/context/CartProvider.js
+++ 6-FoodOrderApp/src/context/CartProvider.js
@@ -1,13 +1,49 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = props => {
-	const addItemHandler = item => {};
+const defaultCartState = {
+	cart: [],
+	totalAmount: 0,
+};
 
-	const removeItemHandler = id => {};
+const cartReducer = (state, action) => {
+	if (action.type === "ADD_ITEM") {
+		const updatedCart = state.cart.concat(action.payload);
+		const updatedTotalAmount =
+			state.totalAmount + action.payload.price * action.payload.amount;
+		return {
+			cart: updatedCart,
+			totalAmount: updatedTotalAmount,
+		};
+	} else if (action.type === "REMOVE_ITEM") {
+	}
+	return defaultCartState;
+};
+
+const CartProvider = props => {
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
+
+	const addItemHandler = item => {
+		dispatchCartAction({
+			type: "ADD_ITEM",
+			payload: item,
+		});
+	};
+
+	const removeItemHandler = id => {
+		dispatchCartAction({
+			type: "REMOVE_ITEM",
+			payload: id,
+		});
+	};
 
 	const cartContext = {
-		cart: [],
-		totalAmount: 0,
+		cart: cartState.cart,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemHandler,
 		removeItem: removeItemHandler,
 	};
