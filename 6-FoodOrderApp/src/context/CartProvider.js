d3977e695683903487e6c1ee35d5c5ced41afeb8
Spencer Heffley <spheffley@yahoo.com>
2022-07-27 02:09:02 -0400

    Changed logic in addItem function to only update the amount of an item if it is already in the cart

--- 6-FoodOrderApp/src/context/CartProvider.js
+++ 6-FoodOrderApp/src/context/CartProvider.js
@@ -9,9 +9,29 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === "ADD_ITEM") {
-		const updatedCart = state.cart.concat(action.payload);
 		const updatedTotalAmount =
 			state.totalAmount + action.payload.price * action.payload.amount;
+
+		const existingCartItemIndex = state.cart.findIndex(
+			item => item.id === action.payload.id
+		);
+
+		const existingCartItem = state.cart[existingCartItemIndex];
+
+		let updatedCart;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.payload.amount,
+			};
+
+			updatedCart = [...state.cart];
+			updatedCart[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedCart = state.cart.concat(action.payload);
+		}
+
 		return {
 			cart: updatedCart,
 			totalAmount: updatedTotalAmount,
