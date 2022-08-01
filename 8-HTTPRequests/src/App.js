14fa022e2fc27fe579c6bbc7e78d7f41d257583a
Spencer Heffley <spheffley@yahoo.com>
2022-08-01 18:59:19 -0400

    Added loading state to show when the application is loading movies

--- 8-HTTPRequests/src/App.js
+++ 8-HTTPRequests/src/App.js
@@ -5,19 +5,22 @@ import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	async function fetchMoviesHandler() {
-		const response = await fetch("https://swapi.dev/api/films")
+		setLoading(true);
+		const response = await fetch("https://swapi.dev/api/films");
 		const data = await response.json();
-        const transformedMovies = data.results.map(movie => {
-            return {
-                id: movie.episode_id,
-                title: movie.title,
-                releaseDate: movie.release_date,
-                openingText: movie.opening_crawl,
-            };
-        });
-        setMovies(transformedMovies);
+		const transformedMovies = data.results.map(movie => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				releaseDate: movie.release_date,
+				openingText: movie.opening_crawl,
+			};
+		});
+		setMovies(transformedMovies);
+		setLoading(false);
 	}
 
 	return (
@@ -26,7 +29,9 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{loading && <h2>Loading...</h2>}
+				{!loading && movies.length === 0 && <h2>No Movies Fetched</h2>}
+				{!loading && movies.length > 0 && <MoviesList movies={movies} />}
 			</section>
 		</React.Fragment>
 	);
