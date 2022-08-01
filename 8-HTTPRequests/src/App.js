9b98ac64d49f944d6eff87eecb9a035b5b9082d3
Spencer Heffley <spheffley@yahoo.com>
2022-08-01 18:49:50 -0400

    Changed function to async function and used await instead of .then blocks

--- 8-HTTPRequests/src/App.js
+++ 8-HTTPRequests/src/App.js
@@ -6,20 +6,18 @@ import "./App.css";
 function App() {
 	const [movies, setMovies] = useState([]);
 
-	function fetchMoviesHandler() {
-		fetch("https://swapi.dev/api/films")
-			.then(response => response.json())
-			.then(data => {
-				const transformedMovies = data.results.map(movie => {
-					return {
-						id: movie.episode_id,
-						title: movie.title,
-						releaseDate: movie.release_date,
-						openingText: movie.opening_crawl,
-					};
-				});
-				setMovies(transformedMovies);
-			});
+	async function fetchMoviesHandler() {
+		const response = await fetch("https://swapi.dev/api/films")
+		const data = await response.json();
+        const transformedMovies = data.results.map(movie => {
+            return {
+                id: movie.episode_id,
+                title: movie.title,
+                releaseDate: movie.release_date,
+                openingText: movie.opening_crawl,
+            };
+        });
+        setMovies(transformedMovies);
 	}
 
 	return (
