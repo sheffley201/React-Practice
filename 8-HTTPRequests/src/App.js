9d6b3af011ed4ddd9645df5d1a04801f478239cc
Spencer Heffley <spheffley@yahoo.com>
2022-08-02 22:23:04 -0400

    Added files to prepare for doing a POST request

--- 8-HTTPRequests/src/App.js
+++ 8-HTTPRequests/src/App.js
@@ -1,51 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
 	const [movies, setMovies] = useState([]);
-	const [loading, setLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	async function fetchMoviesHandler() {
-		setLoading(true);
+	const fetchMoviesHandler = useCallback(async () => {
+		setIsLoading(true);
 		setError(null);
 		try {
 			const response = await fetch("https://swapi.dev/api/films/");
-
 			if (!response.ok) {
-				throw new Error("Something went wrong");
+				throw new Error("Something went wrong!");
 			}
 
 			const data = await response.json();
-			const transformedMovies = data.results.map(movie => {
+
+			const transformedMovies = data.results.map(movieData => {
 				return {
-					id: movie.episode_id,
-					title: movie.title,
-					releaseDate: movie.release_date,
-					openingText: movie.opening_crawl,
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
 				};
 			});
 			setMovies(transformedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
-		setLoading(false);
+		setIsLoading(false);
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
+
+	function addMovieHandler(movie) {
+		console.log(movie);
 	}
 
-	let content = <h2>No Movies Fetched</h2>;
+	let content = <p>Found no movies.</p>;
 
-	if (loading) {
-		content = <h2>Loading...</h2>;
-	} else if (!loading && error) {
-		content = <h2>{error}</h2>;
-	} else if (!loading && !error && movies.length > 0) {
+	if (movies.length > 0) {
 		content = <MoviesList movies={movies} />;
+	}
+
+	if (error) {
+		content = <p>{error}</p>;
+	}
+
+	if (isLoading) {
+		content = <p>Loading...</p>;
 	}
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
