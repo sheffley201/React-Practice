dd084b1f4be1601b09fd05ca03e6c3fb4e4f8ae2
Spencer Heffley <spheffley@yahoo.com>
2022-08-04 17:31:05 -0400

    Formatted files

--- 9-CustomHooks/src/components/ForwardCounter.js
+++ 9-CustomHooks/src/components/ForwardCounter.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Card from './Card';
+import Card from "./Card";
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+	const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCounter(prevCounter => prevCounter + 1);
+		}, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+		return () => clearInterval(interval);
+	}, []);
 
-  return <Card>{counter}</Card>;
+	return <Card>{counter}</Card>;
 };
 
 export default ForwardCounter;
